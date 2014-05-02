5c1dab9fc781f651442e8a67c7e9d5bdcf0f6296
gaso <gustavo2840@hotmail.com>
2014-05-01 20:15:40 -0500

    inicio con estudiante universidad

---
+++ models/estudiante.py
@@ -0,0 +1,20 @@
+#-*- encoding utf-8 -*-
+from osv import osv,fields
+
+class estudiante(osv.Model):
+	_name = 'gid.cursos.estudiante'
+	
+
+	_columns = {
+		
+		'name':fields.char('Nombre',required=True,size=200),
+		'identificacion' : fields.char(u'Identificación', size=11 ),
+		'direccion':fields.char(u'Dirección',required=True,size=100),
+		'telefono':fields.char(u'Teléfono',required=True,size=100),
+		'email':fields.char(u'E-mail',required=True,size=100),
+		'active':fields.boolean('Active'),
+
+	}
+	_defaults={
+		'active' : True,
+	}
