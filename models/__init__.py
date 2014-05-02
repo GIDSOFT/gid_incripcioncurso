f7a5460e97bb65cdb19f765b11774241f46b88d0
gaso <gustavo2840@hotmail.com>
2014-05-01 20:51:48 -0500

    relacion universidad id y modelo docente

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 import universidad
 import estudiante
 import curso
+import docente
--- models/docente.py
+++ models/docente.py
@@ -3,11 +3,12 @@ from osv import osv,fields
 
 class docente  (osv.Model):
 	_name ='gid.cursos.docente'
-	_colums= {
+
+	_columns= {
 		'name':fields.char('Nombre',required=True,size=200),
 		'identificacion': fields.char('iden',required=True,size=300),
-		'correo': fields.varchar('email',required=True,size=300),
+		'correo': fields.char('email',required=True,size=300),
 		'telefono': fields.char('tel',required=True,size=200),
 		'active':fields.boolean('active'),
-		# 'universidad_id':fields.char('universidad',reqired=true, size=200),
+		'universidad_id':fields.many2one('gid.cursos.universidad',required=True, size=200),
 	}
